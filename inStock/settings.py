d393a8c7c1b604b8ef22da67c3ed7d7eddeec65e
romulogss <romulo.gabss@gmail.com>
2019-11-22 15:33:36 -0300

    Deploy

--- inStock/settings.py
+++ inStock/settings.py
@@ -127,4 +127,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 import django_heroku
+
 django_heroku.settings(locals())
--- inStock/urls.py
+++ inStock/urls.py
@@ -39,14 +39,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [
-                      path('__debug__/', include(debug_toolbar.urls)),
-
-                      # For django versions before 2.0:
-                      # url(r'^__debug__/', include(debug_toolbar.urls)),
-
-                  ] + urlpatterns
