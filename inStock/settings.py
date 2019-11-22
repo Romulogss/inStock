12634fa2ca25d9491445ef63b7a8b6fb0ebb1e4d
romulogss <romulo.gabss@gmail.com>
2019-11-22 18:41:30 -0300

    outro teste

--- inStock/settings.py
+++ inStock/settings.py
@@ -53,8 +53,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'inStock.urls'
 
