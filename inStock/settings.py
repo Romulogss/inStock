746c048e021f75aeb56ec7c6a0db78d6a23c2d3f
romulogss <romulo.gabss@gmail.com>
2019-11-29 17:56:34 -0300

    novo banco

--- inStock/settings.py
+++ inStock/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'inStock.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+DATABASES = {'default': config(default_dburl, default=default_dburl, cast=dburl), }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
