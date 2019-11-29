5ec69e73c210ee6d882d969501732785bea3f2f9
Joswells <wagnerjr300@gmail.com>
2019-11-29 15:38:28 -0300

    Novo banco de Dados

--- estoque/serializers.py
+++ estoque/serializers.py
@@ -6,13 +6,14 @@ from .models import *
 
 
 class LoteSerializer(ModelSerializer):
-    produtos = PrimaryKeyRelatedField(many=True, queryset=Produto.objects.all())
+
     class Meta:
         model = Lote
         fields = '__all__'
 
 
 class ProdutoSerializer(ModelSerializer):
+    lotes = PrimaryKeyRelatedField(many=True, queryset=Lote.objects.all())
     class Meta:
         model = Produto
         fields = '__all__'
