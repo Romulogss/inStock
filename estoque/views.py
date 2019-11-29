4815eb761e963c0360ea04ed001b3d523cc466a1
Joswells <wagnerjr300@gmail.com>
2019-11-29 15:39:40 -0300

    Novo banco de Dados

--- estoque/views.py
+++ estoque/views.py
@@ -50,28 +50,10 @@ class ProdutoList(ListCreateAPIView):
     no lote
     """
 
-    def post(self, request, *args, **kwargs):
-        produto = request.data
-        lote = Lote.objects.get(pk=int(produto['lote']))
-        if lote.quantidade is not None:
-            lote.quantidade += 1
-        else:
-            lote.quantidade = 1
-        lote.save()
-        return self.create(request, *args, **kwargs)
-
 
 class ProdutoDetail(RetrieveUpdateDestroyAPIView):
     queryset = Produto.objects.all().order_by('nome')
     serializer_class = ProdutoSerializer
-
-    def delete(self, request, *args, **kwargs):
-        produto = kwargs.get('pk')
-        produto = Produto.objects.get(pk=produto)
-        lote = produto.lote
-        lote.quantidade -= 1
-        lote.save()
-        return self.destroy(request, *args, **kwargs)
 
 
 class ProdutoBusca(APIView):
